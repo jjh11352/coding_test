b3ee3adfeab58d469cd008b17fad7a8e1ff04178
AURORA57 <96759785+jjh11352@users.noreply.github.com>
2026-02-10 19:31:43 +0900

    [level 0] Title: l로 만들기, Time: 0.50 ms, Memory: 3.89 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    for(int i=0;i<myString.length();i++){
+        if(myString[i] < 'l')
+            myString[i]  = 'l';
+    }
+    return myString;
+}

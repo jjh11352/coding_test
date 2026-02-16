5df7a1c3e206961ecb1f738cd54c5dd243a3a688
AURORA57 <96759785+jjh11352@users.noreply.github.com>
2026-02-16 17:15:14 +0900

    [level 0] Title: 배열 비교하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+int solution(vector<int> arr1, vector<int> arr2) {
+    int answer = 0;
+    if(arr1.size()>arr2.size())
+        return 1;
+    else if(arr1.size()<arr2.size())
+        return -1;
+    else{
+        if(accumulate(arr1.begin(), arr1.end(), 0) > accumulate(arr2.begin(), arr2.end(), 0))
+            return 1;
+        else if(accumulate(arr1.begin(), arr1.end(), 0) < accumulate(arr2.begin(), arr2.end(), 0))
+            return -1;
+        else
+            return 0;
+    }
+    return answer;
+}

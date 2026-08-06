3e7d6dc268e8ecbbe88d6b868e7a6a22c51a93ed
AURORA57 <96759785+jjh11352@users.noreply.github.com>
2026-08-06 16:39:06 +0900

    [level 2] Title: H-Index, Time: 0.13 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,12 @@
+def solution(citations): 
+    h = 0
+    n = len(citations)
+    citations.sort(reverse=True)
+    
+    for i in range(len(citations)):
+        if citations[i] >= i + 1:
+            h = i + 1
+        else:
+            break
+    
+    return h
